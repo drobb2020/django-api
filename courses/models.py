fdb798cd3b9d243b0c47bbc1f6995adbbad4505f
David Robb <66834033+drobb2020@users.noreply.github.com>
2023-03-03 20:09:20 -0500

    Initial commit

---
+++ courses/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    language = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.language}"
